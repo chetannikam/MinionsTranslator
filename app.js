5f56190a2fc287c38bfa51a42b88500e42da9129
chetannikam <nikamchetan1998@gmail.com>
2021-09-18 00:41:42 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var button=document.querySelector("#btn")
 var inputarea=document.querySelector("#input-area")
-var output=document.querySelector("#output")
+var output=document.querySelector("#output-textarea")
 // var outputarea=document.querySelector("#output-area")
+
+function constructURL(url,user_input)
+{
+    return url+"?"+"text="+user_input;
+}
+function errorhandlerfun(para)
+{ 
+    console.log("Server having problem try after sometime:-"+para)
+}
 function what_happens_after_click()
 {
-// outputarea.innerHTML=inputarea.value
-output.innerText=inputarea.value;
+
+var url="https://api.funtranslations.com/translate/minion.json";
+var final_url=constructURL(url,inputarea.value)
+fetch(final_url).then(response=>response.json()).then(json=>{var op=json.contents.translated;
+    output.innerText=op;
+
+}).catch(errorhandlerfun)
 };
 button.addEventListener("click",what_happens_after_click)
 
